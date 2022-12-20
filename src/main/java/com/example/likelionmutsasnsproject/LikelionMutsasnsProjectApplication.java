f0dc84591a1a1a2030cf70a4554fa75e5c09d232
JO YEJI <whdpwl2@gmail.com>
2022-12-20 16:06:51 +0900

    init: project build

---
+++ src/main/java/com/example/likelionmutsasnsproject/LikelionMutsasnsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionmutsasnsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionMutsasnsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionMutsasnsProjectApplication.class, args);
+    }
+
+}
