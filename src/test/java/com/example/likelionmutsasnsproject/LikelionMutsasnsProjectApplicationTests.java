c2b9d4b8a8a63eca10ca3bb05a17af07d1f0e9fc
JO YEJI <whdpwl2@gmail.com>
2022-12-20 16:59:44 +0900

    test: 기본 테스트 수정

--- src/test/java/com/example/likelionmutsasnsproject/LikelionMutsasnsProjectApplicationTests.java
+++ src/test/java/com/example/likelionmutsasnsproject/LikelionMutsasnsProjectApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.likelionmutsasnsproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LikelionMutsasnsProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class LikelionMutsasnsProjectApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
