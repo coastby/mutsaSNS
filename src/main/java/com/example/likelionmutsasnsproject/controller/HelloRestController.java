0589fdd9c87f0a6c742633bc86265962dcce86f1
JO YEJI <whdpwl2@gmail.com>
2022-12-27 15:36:00 +0900

    :memo: docs: add swagger annotation

--- src/main/java/com/example/likelionmutsasnsproject/controller/HelloRestController.java
+++ src/main/java/com/example/likelionmutsasnsproject/controller/HelloRestController.java
@@ -1,5 +1,6 @@
 package com.example.likelionmutsasnsproject.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRestController {
     @GetMapping(value = "/hello")
     public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok().body("조예지");
+        return ResponseEntity.ok().body("hello");
     }
+    @Operation(summary = "security 테스트용", description = "USER 권한 이용자만 접근 가능 (포스트 작성과 동일)")
     @GetMapping(value = "/auth-test-api")
     public ResponseEntity<String> sayGood() {
         return ResponseEntity.ok().body("good");
